069285ce1c53cb01125a0f434348995fdb9275cb
qingli <liqing51269@gmail.com>
2022-08-12 12:08:32 -0400

    Initialize spring boot project structure with get endpoints (#10)
    * Initialize spring boot project structure with get endpoints for user entity
    
    * Add unit tests for UserController and UserService

---
+++ src/main/java/furiends/backend/controller/UserController.java
@@ -0,0 +1,31 @@
+package furiends.backend.controller;
+
+import furiends.backend.model.User;
+import furiends.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("getUser/{userId}")
+    public ResponseEntity<Optional<User>> get(@PathVariable("userId")String userId) throws NoSuchElementException{
+        return ResponseEntity.ok(userService.findUserById(userId));
+
+    }
+
+    @GetMapping("getAllUsers")
+    public ResponseEntity<ArrayList<User>> getAll() throws RuntimeException {
+        return ResponseEntity.ok(userService.findAllUsers());
+    }
+}
---
+++ src/test/java/furiends/backend/service/UserServiceTest.java
@@ -0,0 +1,54 @@
+package furiends.backend.service;
+
+import furiends.backend.model.User;
+import furiends.backend.repository.UserRepository;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@SpringBootTest
+class UserServiceTest {
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    private User mockUser = new User("test_user_1",1,true,
+            "12345",false);
+
+    private ArrayList<User> mockUserList = new ArrayList<User>() {{
+        add(mockUser);
+    }};
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testFindAllUsers() {
+        when(userRepository.findAll()).thenReturn(mockUserList);
+        ArrayList<User> userList = userService.findAllUsers();
+        Assert.assertEquals(userList.size(), mockUserList.size());
+        Assert.assertEquals(userList.get(0).getWechatId(), mockUserList.get(0).getWechatId());
+    }
+
+    @Test
+    void testFindUserById() {
+        when(userRepository.findById("test_user_1")).thenReturn(java.util.Optional.ofNullable(mockUser));
+        Optional<User> user = userService.findUserById("test_user_1");
+        Assert.assertEquals(user.get().getWechatId(), mockUser.getWechatId());
+    }
+}
