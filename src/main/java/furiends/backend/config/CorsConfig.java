e8309c1da9f8c6d4dba17ae3c09e641568b4d05d
778789583@qq.com <64758670+anxi@users.noreply.github.com>
2022-10-08 09:15:51 +0800

    feature:Cross domain request configuration

--- src/main/java/furiends/backend/config/CorsConfig.java
+++ src/main/java/furiends/backend/config/CorsConfig.java
@@ -6,28 +6,10 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-//@Configuration
-////public class CorsConfig {
-////
-////    // 当前跨域请求最大有效时长。这里默认1天
-////    private static final long MAX_AGE = 24 * 60 * 60;
-////
-////    @Bean
-////    public CorsFilter corsFilter() {
-////        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-////        CorsConfiguration corsConfiguration = new CorsConfiguration();
-////        corsConfiguration.addAllowedOrigin("*"); // 1 设置访问源地址
-////        corsConfiguration.addAllowedHeader("*"); // 2 设置访问源请求头
-////        corsConfiguration.addAllowedMethod("*"); // 3 设置访问源请求方法
-////        corsConfiguration.setMaxAge(MAX_AGE);
-////        source.registerCorsConfiguration("/**", corsConfiguration); // 4 对接口配置跨域设置
-////        return new CorsFilter(source);
-////    }
-////}
 @Configuration
 public class CorsConfig {
 
-    private CorsConfiguration buildconfig(){
+    private CorsConfiguration buildConfig(){
         CorsConfiguration configuration = new CorsConfiguration();
         //设置请求头，*代表所有
         configuration.addAllowedHeader("*");
@@ -43,7 +25,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter(){
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**",buildconfig());
+        source.registerCorsConfiguration("/**",buildConfig());
         return new CorsFilter(source);
     }
 }
