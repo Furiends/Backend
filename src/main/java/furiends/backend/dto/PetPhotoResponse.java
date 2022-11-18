426b282f4f654364328da68476b071a81edcb4af
qingli <liqing51269@gmail.com>
2022-11-18 00:12:24 -0500

    Refactor petPhotoResponse and controller

--- src/main/java/furiends/backend/dto/PetPhotoResponse.java
+++ src/main/java/furiends/backend/dto/PetPhotoResponse.java
@@ -8,8 +8,16 @@ public class PetPhotoResponse {
 
     private List<String> petPhotoUrlList;
 
+    public String getPetId() {
+        return petId;
+    }
+
     public void setPetId(String petId) {
         this.petId = petId;
+    }
+
+    public List<String> getPetPhotoUrlList() {
+        return petPhotoUrlList;
     }
 
     public void setPetPhotoUrlList(List<String> petPhotoUrlList) {
